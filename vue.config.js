81770bc8258838c8abf8f32f3e63a7792037664b
coseo12 <momo_12@naver.com>
2020-01-09 19:53:01 +0900

    build path

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,11 @@
 module.exports = {
-  "publicPath": "/",
-  "devServer": {
-    "open": true,
-    "host": "127.0.0.1",
-    "port": 3000
+  pwa: {
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      swSrc: 'src/service-worker.js',
+    },
   },
-  "pwa": {
-    "appleMobileWebAppCapable": "yes",
-    "appleMobileWebAppStatusBarStyle": "black",
-    "workboxPluginMode": "InjectManifest",
-    "workboxOptions": {
-      "swSrc": "src/service-worker.js"
-    }
-  },
-  "transpileDependencies": [
-    "vuetify"
-  ]
-}
+  transpileDependencies: ['vuetify'],
+};
