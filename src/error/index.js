779759e1bca26a0e65990042412a9a4a3f28dd4c
coseo12 <momo_12@naver.com>
2020-01-17 16:16:00 +0900

    add

--- src/error/index.js
+++ src/error/index.js
@@ -3,5 +3,6 @@ import typeAlerts from '../utils/typeAlerts'
 export default (store, msg, error) => {
   // type : success, info, warningm error
   typeAlerts(store, msg, 'error');
-  console.log(error);
+  error
+  // console.log(error);
 };
