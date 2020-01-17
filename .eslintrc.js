61969de9c0f43d98e3732d86a7ec9f872947d298
coseo12 <momo_12@naver.com>
2020-01-17 16:13:38 +0900

    add

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   extends: ['plugin:vue/essential', 'eslint:recommended'],
   rules: {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
   parserOptions: {
     parser: 'babel-eslint',
